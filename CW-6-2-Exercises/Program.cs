31eabfe5b745399d00f52ba4dfb8b72629bee2e2
EM80AFR <emadafrasiyabi14@gmail.com>
2023-01-06 14:52:21 +0330

    initialize-project

---
+++ CW-6-2-Exercises/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CW_6_2_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString =Console.ReadLine();
+            PrintMassage(inputString);
+
+        }
+        public static void PrintMassage(string inputStr)
+        {
+            Console.WriteLine($"{inputStr}");
+        }
+    }
+   
+}
+
+
